48636c2f3c97c46c9aa2b3dc1c1491767ad05a77
vansh <vanshthapa14988@gmail.com>
2026-03-10 21:37:40 +0530

    updated api url for production

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/api"
+  baseURL: "https://sql-studio-backend-jjip.onrender.com"
 });
 
 export default api;
